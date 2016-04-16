c4610891e31caad86d574caba96d2914b6d7eaab
devel <devel@develX455L>
2016-04-16 20:28:19 -0300

    test

--- src/main/java/com/sis/test/MaintTest.java
+++ src/main/java/com/sis/test/MaintTest.java
@@ -1,8 +1,14 @@
 package com.sis.test;
 
+import java.util.Date;
+
 public class MaintTest {
 
 	public String saludar(){
-		return "Hola Sistema de Turnos Automatico";
+		return "Hola Sistema de Turnos Automatico.";
+	}
+	
+	public void imprimirDia(){
+		System.out.println("La hora es: " + new Date());
 	}
 }
